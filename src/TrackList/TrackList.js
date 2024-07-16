3a8f1f03864c05e59f5640bec0faac39e73c1860
Yuriy Burylo <burylojunior@gmail.com>
2024-07-16 13:43:22 +0300

    used props destructuring in TrackList component for syntax simplification

--- src/TrackList/TrackList.js
+++ src/TrackList/TrackList.js
@@ -2,12 +2,12 @@ import React from 'react';
 import Track from '../Track/Track';
 import Button from '../Button/Button';
 
-function TrackList(props) {
-    const tracksArray = props.list;
+function TrackList({list, button, handleClick}) {
+    const tracksArray = list;
     return (
         <ol>
             {
-                tracksArray.map((track, index) => <li key={index}><Track object={track} /><Button identifier={track.id} handleClick={props.handleClick}>{props.button}</Button></li>)
+                tracksArray.map((track, index) => <li key={index}><Track object={track} /><Button identifier={track.id} handleClick={handleClick}>{button}</Button></li>)
             }
         </ol>
     );
