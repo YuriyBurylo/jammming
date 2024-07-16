4f9d275ae2a4ace9e95ca3642545008ca28de84f
Yuriy Burylo <burylojunior@gmail.com>
2024-07-16 13:48:11 +0300

    further optimization of syntax in TrackList component

--- src/TrackList/TrackList.js
+++ src/TrackList/TrackList.js
@@ -3,11 +3,10 @@ import Track from '../Track/Track';
 import Button from '../Button/Button';
 
 function TrackList({list, button, handleClick}) {
-    const tracksArray = list;
     return (
         <ol>
             {
-                tracksArray.map((track, index) => <li key={index}><Track object={track} /><Button identifier={track.id} handleClick={handleClick}>{button}</Button></li>)
+                list.map((track, index) => <li key={index}><Track object={track} /><Button identifier={track.id} handleClick={handleClick}>{button}</Button></li>)
             }
         </ol>
     );
